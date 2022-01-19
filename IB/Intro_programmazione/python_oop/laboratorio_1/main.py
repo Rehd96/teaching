527c5b733dacdcc171ccd8716aa0db14dedf748a
Rehd96 <ionut.dodoiu@gmail.com>
2022-01-19 10:34:33 +0100

    Beh si insomma, si poteva fare

--- IB/Intro_programmazione/python_oop/laboratorio_1/main.py
+++ IB/Intro_programmazione/python_oop/laboratorio_1/main.py
@@ -1,26 +1,33 @@
 # How to create a class:
 class Item:
-    def calculate_total_price(self, x, y):
-        return x * y
+    name = ''
+    price = ''
+    quantity = ''
+    
+      
+    def __init__(self,name=0,price=0,quantity=0):
+        self.name = name
+        if price < 0 :
+            raise ValueError("price non può essere minore di 0 ")
+        self.price = price
+        if quantity < 0:
+            raise ValueError("quantity non può essere minore di 0")
+        self.quantity = quantity
+        
+    
+        
+    def calculate_total_price(self):
+        return self.price * self.quantity
 
 # How to create an instance of a class
-item1 = Item()
-
-# Assign attributes:
-item1.name = "Phone"
-item1.price = 100
-item1.quantity = 5
+item1 = Item("Phone",100,5)
 
 # Calling methods from instances of a class:
-print(item1.calculate_total_price(item1.price, item1.quantity))
+print(item1.calculate_total_price())
 
 # How to create an instance of a class (We could create as much as instances we'd like to)
-item2 = Item()
+item2 = Item("laptop",1000,3)
 
-# Assign attributes
-item2.name = "Laptop"
-item2.price = 1000
-item2.quantity = 3
 
 # Calling methods from instances of a class: 
-print(item2.calculate_total_price(item2.price, item2.quantity))
+print(item2.calculate_total_price())
